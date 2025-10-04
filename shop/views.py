3970c4d0ec4f51a78e3847a6b43de907f9f0da98
bkh1423 <bkh14231423@gmail.com>
2025-10-05 02:59:32 +0300

    تعديل ملف الviews

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1>مرحبًا بك في المتجر الإلكتروني 🌸</h1>")
