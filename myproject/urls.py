33e9bfd22c3a33b8ee1367b1428bcbf4980d56c7
bkh1423 <bkh14231423@gmail.com>
2025-10-05 02:56:59 +0300

    تعديل ال urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include  
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),  
-    path('shop/', include('shop.urls')),          
-    path('orders/', include('orders.urls')),      
+
+    # روابط التطبيقات
+    path('accounts/', include('accounts.urls')),   # تسجيل ودخول وبروفايل
+    path('shop/', include('shop.urls')),           # المنتجات والعربة
+    path('orders/', include('orders.urls')),       # الطلبات والدفع
 ]
+
+# 📌 إضافة دعم عرض ملفات media أثناء التطوير
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
