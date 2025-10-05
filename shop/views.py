448a8899e1567faeb03c255e5f5ddeb5fe252e51
bkh1423 <bkh14231423@gmail.com>
2025-10-05 18:40:18 +0300

    تعديل ال views حق المنتجات

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
+
+# الصفحة الرئيسية (موجودة عندك سابقاً)
 def home(request):
     return render(request, 'home.html')
+
+
+# عرض كل المنتجات
+def product_list(request):
+    products = Product.objects.all().select_related('category')
+    return render(request, 'shop/product_list.html', {'products': products})
