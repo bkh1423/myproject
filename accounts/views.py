733016e63219df4918b0bc56725d14b44f57c1bd
bkh1423 <bkh14231423@gmail.com>
2025-10-05 15:18:20 +0300

    تعديل الفيو

--- accounts/views.py
+++ accounts/views.py
@@ -1,34 +1,33 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
-# تسجيل مستخدم جديد
+
 def register_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # تسجيل الدخول مباشرة بعد إنشاء الحساب
-            return redirect("/")  # رجّعه للصفحة الرئيسية أو أي مسار آخر
+            login(request, user)
+            return redirect("/")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, "accounts/register.html", {"form": form})
 
 
-# تسجيل دخول
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
+        form = CustomAuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect("/")
     else:
-        form = AuthenticationForm()
+        form = CustomAuthenticationForm()
     return render(request, "accounts/login.html", {"form": form})
 
 
-# تسجيل خروج (إضافة اختيارية)
 def logout_view(request):
     logout(request)
     return redirect("/")
+
