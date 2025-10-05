2b7e757892ce5462260f097b27ea5065fa348ddc
bkh1423 <bkh14231423@gmail.com>
2025-10-05 14:35:51 +0300

    تعديل الurls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # روابط تطبيق الحسابات ستوضع هنا لاحقًا
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),  # اختياري
 ]
+
