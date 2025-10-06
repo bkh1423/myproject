61d51ceb5aa71dce2b8c7ebad98b3f54d0f19b33
bkh1423 <bkh14231423@gmail.com>
2025-10-06 14:40:56 +0300

    تعديل كود الmodels

--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField   # ✅ استيراد CloudinaryField
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="categories/", blank=True, null=True)  # ✅ صورة للتصنيف
+    image = CloudinaryField("image", folder="categories", blank=True, null=True)  # ✅ صورة من Cloudinary
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -15,9 +16,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
-    image = models.ImageField(upload_to="products/", blank=True, null=True)  # ✅ صورة للمنتج
+    image = CloudinaryField("image", folder="products", blank=True, null=True)  # ✅ صورة من Cloudinary
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-
